25fa5e96b2e6f40e90201b7bc3fdde85a84aa47a
manuel <manuel.larrota@gmail.com>
2022-10-16 09:57:04 -0300

    Minor updates

--- src/main/java/com/zara/prices/infraestructure/ErrorResponseDto.java
+++ src/main/java/com/zara/prices/infraestructure/ErrorResponseDto.java
@@ -10,31 +10,25 @@ public class ErrorResponseDto {
     private final Integer status;
     private final String timeStamp;
     private String message;
-    private String error;
     private String trace;
 
     public ErrorResponseDto(Integer status, Map<String, Object> errorAttributes, String profile) {
-
         this.status = status;
-        this.message = errorAttributes.get("message") + "";
-        this.timeStamp = (String) (errorAttributes.get("timeStamp") == null ? new Date().toString() : errorAttributes.get("timeStamp"));
-        if (profile.compareTo("production") != 0) {
-            this.trace = (String) (errorAttributes.get("trace") == null ? "Resource Not Available" : errorAttributes.get("trace"));
-            if (this.message.isEmpty()) {
-                this.message = trace.substring(0, trace.indexOf("at "));
-            }
-        } else {
-            this.trace = "Error 500";
-            this.message = "Sorry will be fixed soon!!.";
+        message = errorAttributes.get("message") + "";
+        timeStamp = (String) (errorAttributes.get("timeStamp") == null ? new Date().toString() : errorAttributes.get("timeStamp"));
+        if (profile.compareTo("production") == 0) {
+            message = "Sorry will be fixed soon!!.";
         }
-        this.error = (String) (errorAttributes.get("exception") == null ? "Internal error Server" : errorAttributes.get("exception"));
-        log.error(message);
+        trace = (String) (errorAttributes.get("trace") == null ? "Resource Not Available" : errorAttributes.get("trace"));
+        if (message.isEmpty()) {
+            message = trace.substring(0, trace.indexOf("at "));
+        }
         log.error("Trace: " + errorAttributes.get("trace"));
     }
 
     @Override
     public String toString() {
-        return "ErrorResponse{" + "status=" + status + ", error='" + error + '\'' + ", message='" + message
+        return "ErrorResponse{" + "status=" + status + ", message='" + message
                 + '\'' + ", timeStamp='" + timeStamp + '\'' + ", trace='" + trace + '\'' + '}';
     }
 
